c033b3a8f6960d82fc74f9972bf5fc0275929572
ViktorNaumov <viktornaumov2011@mail.ru>
2022-07-29 13:27:14 +0300

    Угадай число

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Угадай число 1-10");
+int a;
+int b;
+Random rnd = new Random();
+b = rnd.Next(1,1);
+a = int.Parse(Console.ReadLine());
+if (b>a)
+{
+    Console.WriteLine("Больше");
+}
+if (b<a)
+{
+  Console.WriteLine("Меньше");
+}
+if (b==a)
+{
+  Console.WriteLine("Победа");
+}
